29178e1689bbb5ec6d825c52c04d7c06864fd058
mengwox <mwh8699329@163.com>
2023-04-13 10:07:14 +0800

    init maven

---
+++ gupao-mall/src/main/java/org/mawenhao/Main.java
@@ -0,0 +1,12 @@
+package org.mawenhao;
+
+/**
+ * TODO
+ *
+ * @author mawenhao 2023/4/13
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
