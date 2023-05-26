0fd977c30f757f9a477f9babe131e4a17b1ca644
mengwox <mwh8699329@163.com>
2023-05-26 19:26:35 +0800

    jdk 8 upgrade to 17

--- mall-service/mall-goods-service/src/main/java/org/mawenhao/goods/GoodsApplication.java
+++ mall-service/mall-goods-service/src/main/java/org/mawenhao/goods/GoodsApplication.java
@@ -3,6 +3,7 @@ package org.mawenhao.goods;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * 启动配置类
---
+++ mall-service/mall-goods-service/src/main/java/org/mawenhao/goods/config/MybatisPlusConfiguration.java
@@ -0,0 +1,35 @@
+package org.mawenhao.goods.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("scan.your.mapper.package")
+public class MybatisPlusConfiguration {
+
+    /**
+     * 新的分页插件
+     * 一缓和二缓遵循mybatis的规则
+     * 需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题(该属性会在旧插件移除后一同移除)
+     *
+     * @since mybatis-plus 3.4.0
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
+        return interceptor;
+    }
+
+    @Bean
+    public ConfigurationCustomizer configurationCustomizer(MybatisPlusInterceptor mybatisPlusInterceptor) {
+        return configuration -> {
+            configuration.addInterceptor(mybatisPlusInterceptor);
+        };
+    }
+}
